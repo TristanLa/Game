856916a3a147891f2b77e11b3d0fef6c27545273
TristanLa <latrasse.tristan@gmail.com>
2020-04-13 18:26:36 +0200

    adding an index

--- Game/urls.py
+++ Game/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from Game import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('belote/', include("belote.urls")),
     path('perudo/', include("perudo.urls")),
     path('toc/', include("toc.urls")),
+    path('', views.home, name="index"),
 ]
